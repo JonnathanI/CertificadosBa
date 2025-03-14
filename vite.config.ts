b2c2a9d8fffa50542215466795cb891dc29ad332
Jonnathan1998 <inaguazojonnathan6@gmail.com>
2025-03-14 12:26:44 -0500

    Certificados Mejora

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5183, // Configura el puerto a 5183
+  },
   css: {
     preprocessorOptions: {
       less: {
